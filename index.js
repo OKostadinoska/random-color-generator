27c5039a5e3e4c720459ecc0643771bbfbc3b06f
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-01-12 15:28:20 +0100

    implement program

---
+++ index.js
@@ -0,0 +1,26 @@
+// importing color libraries from npm
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+// giving a name to the process.argv property
+const hueColor = process.argv[2];
+const luminosityColor = process.argv[3];
+
+// returns a hex code for a random color
+const selectedColor = randomColor({
+  hue: hueColor,
+  luminosity: luminosityColor,
+});
+
+// Use RGB colors in terminal emulators that support it
+console.log(
+  chalk.hex(selectedColor).bold(`############################
+############################
+############################
+#####                 ######
+#####    ${selectedColor}      ######
+#####                 ######
+############################
+############################
+############################`),
+);
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 # Node Random Color Generator
 
-Generates a random color, optionally based on an input
+> Generates a random color, optionally based on an input
+
+Without any user input, print random color:
+
+```bash
+$ node index.js
+Prints random color in a block
+```
+
+With user input of a correct color name: print color:
+It should be able to not only accept the command itself, but also the request for a:
+
+- hue (e.g. green or red or blue)
+- luminosity (e.g. light or dark)
+  ...and then generate random colors that match those choices.
+
+```bash
+$ node index.js
+Prints concrete hue or luminosity in a block
+```
